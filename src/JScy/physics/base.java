3afc0d542175cab551be4ef94dc8a7175bee3b41
Ernie DeFoy <ernie@Ernies-MacBook-Pro.local>
2017-06-08 16:22:41 -0400

    initial commit

--- src/JScy/physics/base.java
+++ src/JScy/physics/base.java
@@ -1,10 +1,7 @@
 package JScy.physics;
 
 /**
- * <p> This class represents a binary tree that represents an expression
- * such that each node itself is an Expression and contains either
- * one or two children, depending on if that Expression is a
- * UnaryOperation or a BinaryOperation, respectively. </p>
+ *
  */
 public class base {
 }
