b000cca4dab11547c448c47e29ddf881e7f2b50c
Ernie DeFoy <ernie@Ernies-MacBook-Pro.local>
2017-06-08 16:33:22 -0400

    added tests for methods parse, tokenize, and readable

--- src/test/ExpressionParserTest.java
+++ src/test/ExpressionParserTest.java
@@ -4,13 +4,8 @@ import JScy.maths.InvalidExpressionException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.ArrayList;
-
 /**
- * <p> This class represents a binary tree that represents an expression
- * such that each node itself is an Expression and contains either
- * one or two children, depending on if that Expression is a
- * UnaryOperation or a BinaryOperation, respectively. </p>
+ *
  */
 public class ExpressionParserTest extends BaseTest {
 
@@ -18,7 +13,7 @@ public class ExpressionParserTest extends BaseTest {
     public void testParse() throws InvalidExpressionException {
 
         final String QUERY = "-5 + 7(x^2) + ((sin(x)";
-        final String EXPECTED = "$5 + 7*(x^2) + ((sin(x)))";
+        final String EXPECTED = "$5+7*(x^2)+((sin(x)))";
         parser.parse(QUERY);
         String actual = parser.getExpression();
 
@@ -29,8 +24,21 @@ public class ExpressionParserTest extends BaseTest {
     public void testTokenizer() throws InvalidExpressionException {
 
         final String QUERY = "sin(x) / 5*x";
-        final String[] EXPECTED = {"sin", "x", "5", "x", "*", "/"};
+        final String[] EXPECTED = {"x", "sin", "5", "x", "*", "/"};
         parser.parse(QUERY);
-        ArrayList<String>
+        final String[] actual = parser.getTokens().toArray(new String[parser.getTokens().size()]);
+System.out.println(actual);
+        Assertions.assertEquals(EXPECTED, actual);
+    }
+
+    @Test
+    public void readable() throws InvalidExpressionException {
+
+        final String QUERY = "5x + 7*x^2";
+        final String EXPECTED = "5x + 7x^2";
+        parser.parse(QUERY);
+        String actual = parser.readable(QUERY);
+
+        Assertions.assertEquals(EXPECTED, actual);
     }
 }
