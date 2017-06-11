67381b6d37cb6d85240c969df5302d61db35a951
Ernie DeFoy <ernie@Ernies-MacBook-Pro.local>
2017-06-10 20:51:49 -0400

    fixed header comment

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
-import JScy.maths.internal.Variable;
-
 /**
  * @author      Ernest DeFoy <erniedefoy@yahoo.com>
- * @version     1.0
  */
 public class Main {
-
-    
-    Variable var = new Variable("x");
 
 }
